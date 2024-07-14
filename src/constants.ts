05588ffb46d15ab0a29d52f8a21bef24ba2aba50
Ditmar <ditmarcastro@gmail.com>
2024-07-13 22:53:48 -0400

    feat: added

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 // -- Constants values
 export const siteCode = "bnkzpghyjk";
 export const featureKey = "bouncing_ball_demo";
-export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
+export const visitorCode = KameleoonUtils.getVisitorCode("ditmar.github.io");
